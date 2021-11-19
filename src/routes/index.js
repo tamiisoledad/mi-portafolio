c66958dea8b5c19aee662a3e00a62fe9f6810fc8
tamiisoledad <tamiisoledad4@gmail.com>
2021-11-19 18:01:35 -0300

    mi portafolio

---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+var {index} = require("../controllers/indexController");
+/* GET home page. */
+router.get('/', index)
+
+module.exports = router;
